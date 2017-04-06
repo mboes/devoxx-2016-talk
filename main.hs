9920cebdb78fb84e6a78bf1a527a3323142edf63
Mathieu Boespflug <m@tweag.io>
2017-04-06 22:06:28 +0200

    source files.

---
+++ main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Data.Int
+import Data.Text (Text)
+import Language.Java
+import Language.Java.Inline
+
+main :: IO Int32
+main = withJVM [] $ do
+    message <- reflect ("Hello World!" :: Text)
+    rc <- [java| { javax.swing.JOptionPane.showMessageDialog(null, $message);
+                   return 0; } |]
+    reify rc
